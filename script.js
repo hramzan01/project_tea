8114e213760a9bdd9bdb0e93a68f365193d3737e
hramzan01 <112130674+hramzan01@users.noreply.github.com>
2023-02-06 22:37:07 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+// var myImage = document.getElementById('mainImage');
+// var imageArray = ['img/teaCup.svg','img/teaBag.svg','img/teaBlack.svg','img/teaMilk.svg','img/teaMix.svg'];
+// var imageIndex = 0
+
+// function changeImage() {
+//     myImage.setAttribute ('src',imageArray[imageIndex]);
+//     imageIndex = (imageIndex + 1) % imageArray.length;
+// }
+
+// document.addEventListener('DOMContentLoaded', function () {
+// 	var button = document.querySelector('#button');
+// 	button.addEventListener('click', function () {
+// 		changeImage();
+// 	});
+// });
+
+// conver the below jquery to vanilla javascript!!
+// $(document).ready(function() {
+//   $("#images_controls").on('click', 'span', function() {
+//     $("#images img").removeClass("opaque");
+
+//     var newImage = $(this).index();
+
+//     $("#images img").eq(newImage).addClass("opaque");
+
+//     $("#images_controls span").removeClass("selected");
+//     $(this).addClass("selected");
+//   });
+// });
