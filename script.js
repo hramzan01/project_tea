ed9ef0d1bce0c5e691830267ef2ec4b8012dd679
hramzan01 <112130674+hramzan01@users.noreply.github.com>
2023-02-09 21:03:12 +0000

    updates

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
-var myImage = document.getElementById('1');
-var imageArray = [1,2,3,4];
-var imageIndex = 0
+var myImage = document.getElementById('startImg');
+var imageArray = [1, 2, 3, 4];
+var imageIndex = 0;
 var button = document.querySelector('#button');
 
 function changeImage() {
-    myImage.classList.remove ('opaque');
-    imageIndex = (imageIndex + 1) % imageArray.length;
+	myImage.classList.remove('opaque');
+	imageIndex = (imageIndex + 1) % imageArray.length;
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-		button.addEventListener('click', function () {
-        console.log(imageIndex);
+	button.addEventListener('click', function () {
+		console.log(imageIndex);
 		changeImage();
 	});
 });
-
-
-
-
-
 
 // convert the below jquery to vanilla javascript!!
 
