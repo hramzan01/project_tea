3347fae64dc985c14399416515823f0142179bcc
hramzan01 <112130674+hramzan01@users.noreply.github.com>
2023-02-17 23:39:35 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,60 +1,60 @@
-var startImage = document.getElementById("startImg");
-var nextImage = document.getElementById("nextImg");
-var bothImages = document.querySelectorAll("img");
-var myStep = document.getElementById("title");
-var myProcess = document.getElementById("process");
+var startImage = document.getElementById('startImg');
+var nextImage = document.getElementById('nextImg');
+var bothImages = document.querySelectorAll('img');
+var myStep = document.getElementById('title');
+var myProcess = document.getElementById('process');
 var imageArray = [
-  "img/teaCup.svg",
-  "img/teaBag.svg",
-  "img/teaBlack.svg",
-  "img/teaMilk.svg",
-  "img/teaMix.svg",
+	'img/teaCup.svg',
+	'img/teaBag.svg',
+	'img/teaBlack.svg',
+	'img/teaMilk.svg',
+	'img/teaMix.svg',
 ];
 var processArray = [
-  "put in the teabag",
-  "pour hot water",
-  "add milk",
-  "mix it up",
-  "get cup",
+	'put in the teabag',
+	'pour hot water',
+	'add milk',
+	'mix it up',
+	'get cup',
 ];
 var stepArray = [
-  "Step Two",
-  "Step Three",
-  "Step Four",
-  "Step Five",
-  "Step One",
+	'Step Two',
+	'Step Three',
+	'Step Four',
+	'Step Five',
+	'Step One',
 ];
 var arrayIndex = 0;
 
 function toggleFade() {
-  bothImages.forEach((img) => {
-    img.classList.toggle("opaque");
-  });
+	bothImages.forEach((img) => {
+		img.classList.toggle('opaque');
+	});
 }
 
 function changeImage() {
-  arrayIndex = (arrayIndex + 1) % imageArray.length;
-  bothImages.forEach((img) => {
-    if (img.classList.contains("opaque")) {
-      img.setAttribute("src", imageArray[arrayIndex]);
-    }
-  });
+	arrayIndex = (arrayIndex + 1) % imageArray.length;
+	bothImages.forEach((img) => {
+		if (img.classList.contains('opaque')) {
+			img.setAttribute('src', imageArray[arrayIndex]);
+		}
+	});
 }
 
 function changeProcess() {
-  myProcess.innerHTML = processArray[arrayIndex];
+	myProcess.innerHTML = processArray[arrayIndex];
 }
 
 function changeStep() {
-  myStep.innerHTML = stepArray[arrayIndex];
+	myStep.innerHTML = stepArray[arrayIndex];
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  var button = document.querySelector("#button");
-  button.addEventListener("click", function () {
-    toggleFade();
-    changeProcess();
-    changeStep();
-    changeImage();
-  });
+document.addEventListener('DOMContentLoaded', function () {
+	var button = document.querySelector('#button');
+	button.addEventListener('click', function () {
+		toggleFade();
+		changeProcess();
+		changeStep();
+		changeImage();
+	});
 });
